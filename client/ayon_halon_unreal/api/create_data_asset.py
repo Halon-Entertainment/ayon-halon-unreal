82b0b154a892cfa45a0026308e49b8c8912598fd
Ballard-dev <williamballard@outlook.com>
2025-03-18 15:56:19 +0000

    Update Logic for data asset creation

--- client/ayon_halon_unreal/api/create_data_asset.py
+++ client/ayon_halon_unreal/api/create_data_asset.py
@@ -1,34 +1,42 @@
 import unreal
 
 from ayon_halon_unreal.api import menu, paths
+from dataclasses import dataclass
 
+@dataclass
+class AyonMetaDataInfo:
+
+    dataclass_parent: str = "DefaultDataAsset"
+    base_path: str = f"/Game/{dataclass_parent}"
+    import_dir: str = "/Game/ImportedData"
+    custom_dataclass: str = "CustomDataAsset"
+    custom_dataclass_dir: str = f"{base_path}.{custom_dataclass}_C"
 
 ## Create a child data asset from the vanilla one
 ## Basic AF
-def create_child_data(child_asset_name="Derp"):
+def create_child_data(new_asset_name="Donut"):
     # Load the primary data asset class
-    # primary_data_asset = unreal.EditorAssetLibrary.load_asset(paths.path_to_vanilla_data_class())
+    # primary_data_asset = unreal.EditorAssetLibrary.load_asset(AyonMetaDataInfo.base_path)
 
-    print(f"{paths.path_to_vanilla_data_class()}")
-    # object_class = unreal.load_object(None, paths.path_to_vanilla_data_class())
-    # asset_class = unreal.EditorAssetLibrary.load_asset(paths.path_to_vanilla_data_class())
+    print(f"{AyonMetaDataInfo.base_path}")
+    # object_class = unreal.load_object(None, AyonMetaDataInfo.base_path)
+    # asset_class = unreal.EditorAssetLibrary.load_asset(AyonMetaDataInfo.base_path)
     data_asset_class = unreal.load_class(
         None,
-        f"{paths.path_to_vanilla_data_class()}.{paths.vanilla_data_name()}_C",
+        f"{AyonMetaDataInfo.base_path}.{AyonMetaDataInfo.dataclass_parent}_C",
     )
 
     if not data_asset_class:
         print(
-            f"Failed to load Blueprint class: {paths.path_to_vanilla_data_class()}"
+            f"Failed to load Blueprint class: {AyonMetaDataInfo.base_path}"
         )
         return
 
-    new_asset_name = f"{child_asset_name}"
-    new_asset_path = f"{paths.data_import_dir()}/{new_asset_name}"
+    new_asset_path = f"{AyonMetaDataInfo.import_dir}/{new_asset_name}"
     # Create the new data asset
     new_asset = unreal.AssetToolsHelpers.get_asset_tools().create_asset(
         asset_name=new_asset_name,
-        package_path=paths.data_import_dir(),
+        package_path=AyonMetaDataInfo.import_dir,
         asset_class=data_asset_class,
         factory=unreal.DataAssetFactory(),
     )
@@ -79,18 +87,26 @@ create_data_asset.create_the_bp()
 # Make the top-end data thingy
 def create_primary_data_asset(child_asset_name="Derp"):
     asset = menu.CustomDataAsset()
-    package_path = paths.path_to_custom_data_class()
-    print(f"Custom Class: {asset}, saving it in: {package_path}")
+
+    print(f"Custom Class: {asset}, saving it in: {AyonMetaDataInfo.custom_dataclass}")
 
     data_asset = unreal.AssetToolsHelpers.get_asset_tools().create_asset(
-        paths.custom_data_name(),  # Asset name
-        paths.path_to_plugin(),  # Folder path
+        AyonMetaDataInfo.custom_dataclass,  # Asset name
+        '/Game',  # Folder path
         unreal.PrimaryDataAsset,  # Class type
         unreal.DataAssetFactory(),  # Generic factory (use a specific one if needed)
     )
     return
 
     data_asset_class = unreal.load_class(
-        None, f"{paths.path_to_custom_data_class()}.{paths.custom_data_name()}"
+        None, f"{AyonMetaDataInfo.custom_dataclass_dir}.{AyonMetaDataInfo.custom_dataclass}"
     )
     return
+
+
+from importlib import reload
+from ayon_halon_unreal.api import create_data_asset
+
+reload(create_data_asset)
+# create_data_asset.create_the_bp()
+create_child_data()
