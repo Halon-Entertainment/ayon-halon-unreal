160e9f62501e4a83c21c9664419833ff52e0b469
Chad Provencher <thechad802@gmail.com>
2025-04-07 14:08:14 -0400

    Update package.py

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "ayon_halon_unreal"
 title = "Halon Unreal"
-version = "0.0.0-halon-0.0.1-dev.8"
+version = "0.0.0-halon-0.0.1-dev.9"
 app_host_name = "unreal"
 client_dir = "ayon_halon_unreal"
 
